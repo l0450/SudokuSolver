24513551776a18352fba0e1e30b1b59666690d26
l0450 <wiktorkumor87@gmail.com>
2025-09-04 15:39:21 +0200

    Creating a sudoku board and setting a number of columns and rows

--- SudokuSolver/src/SudokuSolver.java
+++ SudokuSolver/src/SudokuSolver.java
@@ -1,11 +1,23 @@
-
 public class SudokuSolver {
 	
+	// Initializing the number of columns and rows in a sudoku board
+	private static final int gridsize = 9;
 	
-
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		
-
+		// This multidimensional array represents an actual sudoku board
+		int[][] board = {
+				{7, 0, 2, 0, 5, 0, 6, 0, 0},
+				{0, 0, 0, 0, 0, 3, 0, 0, 0},
+				{1, 0, 0, 0, 0, 9, 5, 0, 0},
+				{8, 0, 0, 0, 0, 0, 0, 9, 0},
+				{0, 4, 3, 0, 0, 0, 7, 5, 0},
+				{0, 9, 0, 0, 0, 0, 0, 0, 8},
+				{0, 0, 9, 7, 0, 0, 0, 0, 5},
+				{0, 0, 0, 2, 0, 0, 0, 0, 0},
+				{0, 0, 7, 0, 4, 0, 2, 0, 3}
+		};
+		
 	}
 
 }
